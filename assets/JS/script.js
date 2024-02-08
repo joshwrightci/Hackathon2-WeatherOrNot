3646a9d056db7f88ff44e3aa7b90229fa0f85a24
Ellis Taylor <ellis.taylor@live.com>
2024-02-08 09:55:27 +0000

    altered an error code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -78,7 +78,7 @@ const getCityCoordinates = () => {
     const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
   
     fetch(API_URL).then(response => response.json()).then(data => {
-        if (!data.length) return alert(`No coordinates found for ${cityName}`);
+        if (!data.length) return alert(`No coordinates found for ${cityName}, please check you spelling and try again.`);
         const { lat, lon, name } = data[0];
         getWeatherDetails(name, lat, lon);
         addCityToList();
